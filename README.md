8e1f155316c9dbdc37e0b60aea2c67110d38d374
Ximena Politi <ximena.politi@gmail.com>
2022-12-14 17:54:02 -0500

    update csv files and README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Proyecto creado con el fin del aprendizaje de Python en un nivel básico
 
 ### En este curso he aprendido
 - Aprende qué significan los errores y cómo manejarlos.
-- Maneja archivos de texto con el context manager.
+- Maneja archivos de texto, csv con el context manager.
 - Preparar un entorno virtual con pip.
 - Utiliza list y dictionary comprehensions, y high order functions.
 - Miniproyecto "juego del ahorcado"
--- archivos_csv.py
+++ archivos_csv.py
@@ -1,18 +1,17 @@
 import csv
 
 
-def dict_whit_data(clave, value):
+def dict_with_data(clave, value):
     iterable = zip(clave, value)
     country_dict = {clave : value for clave, value in iterable}
     return country_dict
 
 
 def read_csv():
-    numbers = []
     with open("./archivos/world_population.csv", "r") as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         header = next(reader)
-        data = list(map(lambda value: dict_whit_data(header, value) ,reader))
+        data = list(map(lambda value: dict_with_data(header, value) ,reader))
         return data
 
 
